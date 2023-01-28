e80762878c1d5ce2e468f73566e17e074e5ff7ea
olnbxxx <117682412+olnbxxx@users.noreply.github.com>
2023-01-28 14:14:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const estacion = "Otoño";
+switch (estacion) {
+  case "Invierno":
+    System.out.println("Es invierno");
+    break;
+  case "Verano":
+    System.out.println("Es Verano");
+    break;
+  case "Otoño":
+    System.out.println("Es otoño");
+    break;
+  case "Primavera":
+    System.out.println("Es primavera");
+    break;
+  default:
+    System.out.println("No es ninguna estación");
+}
